05cc05406f771ada9074854fb81b15686fe0b434
Dalbizurez <dalbizurez@correo.url.edu.gt>
2023-02-14 09:57:01 -0600

    VI Serie
    Se agrego el codigo al  archivo main para crear la carpeta "modulos" y el archivo "prueba.txt" y se creo el archivo gitignore para ignorarla la carpeta "modulos"

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+
+import os
+
 print("Daniel Albizurez Alpirez \n 1637723")
+
+
+mi_ubicacion = os.getcwd()
+if os.path.exists("modulos"):
+    print("La carpeta ya existe")
+else:
+    os.mkdir(mi_ubicacion + "\\modulos")
+    archivo = open('./modulos/prueba.txt', 'w')
+    archivo.write('Hola mundo')
+    archivo.close()
